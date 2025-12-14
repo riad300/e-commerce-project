5af0c958cf98c2471669a116c3da9810348450bd
riad300 <pearlsriad55@gmail.com>
2025-12-14 17:48:04 +0600

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAf2Km1a07ZOLfyvUX8xOWmDkE6Rm7lN90",
+  authDomain: "ecommerce-auth-359dd.firebaseapp.com",
+  projectId: "ecommerce-auth-359dd",
+  appId: "1:345618204792:web:7d693af8b5aff11232458d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
